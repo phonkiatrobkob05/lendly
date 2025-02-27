f4fd4ff4fb6b144a3e8b1d4ccd8414ef0275dbb5
T-Wasutorn <66010758@kmitl.ac.th>
2025-02-28 00:14:26 +0700

    comm33

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Logintest from "./pages/Logintest";
 import NextRegister from "./pages/NextRegister";
 
 function App() {
