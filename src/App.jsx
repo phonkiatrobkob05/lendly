1310390cb33e6a4dd49e2675ddcce84ca76eaf99
Si0dot7 <felm27010@gmail.com>
2025-02-28 23:40:57 +0700

    fix register

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ import { useDispatch } from "react-redux";
 import { login } from "./store/UserSlice";
 import User from "./pages/user/User";
 import Default from "./pages/Default";
+import ItemDetails from './pages/ItemDetails'
+
 function App() {
   const dispatch = useDispatch()
 
