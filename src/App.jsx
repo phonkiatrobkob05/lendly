006442ec64b99b646c37bf9f09e04617930db96e
Si0dot7 <felm27010@gmail.com>
2025-02-28 23:38:12 +0700

    fix register

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,60 @@ import Feed from "./pages/Feed";
 import Library from "./pages/Library";
 import NextRegister from "./pages/NextRegister";
 import Navbar from "./components/Navbar";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { login } from "./store/UserSlice";
+import User from "./pages/user/User";
+import Default from "./pages/Default";
 function App() {
+  const dispatch = useDispatch()
+
+  const idToken = localStorage.getItem('token')
+  const currentUser=async(authtoken)=>{
+    try {
+      const res = await axios.post(import.meta.env.VITE_API_URI + "/currentUser",{},{headers:{authtoken}})
+      console.log('currentUser',res);
+      return res
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const fetchAndDispatchUser=async()=>{
+    if(!idToken){
+      console.log('no token in localstorage');
+    }
+    const userData = await currentUser(idToken)
+    if(userData){
+      dispatch(
+        login({
+        email:userData.data.email,
+        token:idToken
+      }))
+      
+    }
+  }
+  
+  fetchAndDispatchUser()
   return (
     <div className="min-h-screen">
       
 
       {/* Routes */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Profile" element={<Profile />} />
+        {/* public */}
         <Route path="/Login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
+        <Route path="/" element={<Default />} />
+        
+        
+        {/* private */}
+        <Route path="/Home" element={
+          <User>
+            <Home />
+          </User>
+          }/>
+        <Route path="/Profile" element={<Profile />} />
         <Route path="/NextRegister" element={<NextRegister />} />
         <Route path="/Navbar" element={<Navbar />} />
         <Route path="/Feed" element={<Feed />} />
