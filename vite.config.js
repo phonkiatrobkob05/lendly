98887e4c22394b5a17d18980381fbda02eb9bd88
Si0dot7 <felm27010@gmail.com>
2025-03-01 17:42:08 +0700

    add file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
     host: '0.0.0.0', // Allow external access
     allowedHosts: ['lendlyofficial.onrender.com'] // Add your Render domain here
