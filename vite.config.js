4f606e4078cee0ba29f94d5678291661ba5afd98
Si0dot7 <felm27010@gmail.com>
2025-03-01 17:34:48 +0700

    add file

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    port: 3000, // Ensure you're using the correct port
     host: '0.0.0.0', // Allow external access
-    allowedHosts: ['lendly.onrender.com'] // Add your Render domain here
+    allowedHosts: ['lendlyofficial.onrender.com'] // Add your Render domain here
   }
 });
