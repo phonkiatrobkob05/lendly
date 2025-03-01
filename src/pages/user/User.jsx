3e87577d1118cbbbbb11f0a5af6c7609cbb2ed20
Si0dot7 <felm27010@gmail.com>
2025-03-01 12:29:26 +0700

    protect user no error

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,24 +1,14 @@
-import { useEffect } from 'react';
 import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom';
+
 
 
 const User = ({children}) => {
     const {user} = useSelector((state)=>({...state}))
-    const navigate = useNavigate()
     console.log('userRoute',user);
+    
 
-    useEffect(()=>{
-      if(!user || !user.user.token){
-      navigate('/login',{ replace: true })
-    }
-    },[user,navigate])
-
-    if (!user || !user.user.token) {
-      return null; 
-  }
-  
-  return children
+  return user && user.user.token ? children : <div className='bg-gray-400 h-screen flex justify-center'><p className='flex items-center justify-center text-5xl font-bold'>Login!!!!</p></div>
+    
 }
 
 export default User
